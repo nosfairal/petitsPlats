48112f406f0508c3b469c2f888c814b6cec7db81
nosfair <nosfairalchtion@gmail.com>
2023-04-10 10:09:17 +0200

    rename file

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -11,7 +11,7 @@ import {
 } from '../utils/filter.js';
 
 const getRecipes = async () => {
-    const response = await fetch('../../data/recipes.js');
+    const response = await fetch('../../data/recipes.json');
     const recipes = await response.json();
     return recipes;
 };
